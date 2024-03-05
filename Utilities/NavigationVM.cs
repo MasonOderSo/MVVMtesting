4790bfe4f249eee75098b52d574fd01ca3f8dd43
Mason <81445770+MasonOderSo@users.noreply.github.com>
2024-03-05 06:43:47 +0100

    ADD FILES

---
+++ Utilities/NavigationVM.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MVVMtesting.Utilities;
+using System.Windows.Input;
+using MVVMtesting.Properties;
+using MVVMtesting.ViewModel;
+
+namespace MVVMtesting.Utilities
+{
+    class NavigationVM : ViewModelBase
+    {
+        private object _currentView;
+        public object CurrentView
+        {
+            get { return _currentView; }
+            set { _currentView = value; OnPropertyChanged(); }
+        }
+
+        public ICommand Window1Command { get; set; }
+        public ICommand Window2Command { get; set; }
+
+        private void Window1(object obj) => CurrentView = new Window1VM();
+        private void Window2(object obj) => CurrentView = new Window2VM();
+        public NavigationVM()
+        {
+            Window1Command = new RelayCommand(Window1);
+            Window2Command = new RelayCommand(Window2);
+
+            // Startup Page
+            CurrentView = new Window1VM();
+        }
+    }
+}
---
+++ ViewModel/Window2VM.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMtesting.ViewModel
+{
+    internal class Window2VM
+    {
+    }
+}
